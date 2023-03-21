7cd76d4c042728c8841fd3992c85ec04fc366868
jgiaccio <jgiaccio@verbio.com>
2023-03-21 00:25:26 -0300

    transcriber api working

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from typing import Union
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, UploadFile, File
 import whisper
+import time
+import os
 
 app = FastAPI()
 
@@ -9,14 +12,47 @@ app = FastAPI()
 def on_startup():
     global whisper_model
 
-    whisper_model = whisper.load_model(large)
+    whisper_model = whisper.load_model('large')
+
+@app.post("/upload")
+async def receive_file(file: UploadFile = File(...)):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    print(dir_path)
+    filename = f'{dir_path}/uploads/{time.time()}-{file.filename}'
+    with open(filename, 'wb') as f:
+        content = await file.read()
+        f.write(content)
 
 
 @app.post("/transcribe")
-async def download_file(audio_file):
+async def transcribe_file(file: UploadFile = File(...)):
 
-    detected_language, mel = get_language(whisper_model, audio_file)
-    transcription = transcriber(whisper_model, mel)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    print(dir_path)
+    filename = f'{dir_path}/uploads/{time.time()}-{file.filename}'
+    with open(filename, 'wb') as f:
+        content = await file.read()
+        f.write(content)
+
+    #detected_language, mel = get_language(whisper_model, UploadFile.file)
+    #transcription = transcriber(whisper_model, mel)
+    audio = whisper.load_audio(filename)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(whisper_model.device)
+
+    # detect the spoken language
+    _, probs = whisper_model.detect_language(mel)
+    #print(f"Detected language: {max(probs, key=probs.get)}")
+    lang_set = {max(probs, key=probs.get)}
+    # decode the audio
+    options = whisper.DecodingOptions(fp16=False)
+    result = whisper.decode(whisper_model, mel, options)
+
+    transcription = result.text
+
+    print(transcription)
 
     return transcription
 
@@ -48,3 +84,6 @@ def transcriber(model, mel):
     transcription = result.text
 
     return transcription
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
